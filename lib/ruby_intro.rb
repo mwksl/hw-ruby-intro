ae5d3d1f0b616d6a84f0d4b1a333046de58ccd13
Matthew Stingel <Matthew@leaguer.io>
2015-11-03 03:46:07 +0000

    Add parts 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,50 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.sort!
+  return arr[-1] + arr[-2] if arr.length >= 2
+  return arr[0] if arr.length == 1
+  return 0 if arr.length == 0 
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return true if !!arr.uniq.combination(2).detect { |a, b| a + b == n }
+  return false if (arr.empty? or arr.length == 1)
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " << name 
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  puts s[0]
+  vowel_arr = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+  return false if vowel_arr.include? s[0]
+  return false if s.empty? or ( not s[0].match(/[a-zA-Z]/) )
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if not s.match(/^[0-1]+$/)
+  return s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price) 
+    @price = price
+    @isbn  = isbn
+  end
+  
 end
